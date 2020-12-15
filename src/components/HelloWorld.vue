3935a02ecaa0ef826898bfc4b08b4492b2494541
menglei <2234526190@qq.com>
2020-12-15 15:18:15 +0800

    分支合并测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,9 +4,6 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
     </p>
   </div>
 </template>
